15555a87740da029ce52214104927ed1dec8c74a
Zehan Sunesara <usazehan@gmail.com>
2018-07-27 15:07:40 -0500

    Camera roll

--- mobile/src/screens/CreatePhotoScreen/index.js
+++ mobile/src/screens/CreatePhotoScreen/index.js
@@ -1,47 +1,105 @@
 import React, {PureComponent} from 'react';
-import {StyleSheet, View, Text, CameraRoll, Image, FlatList} from 'react-native';
+import {StyleSheet, View, CameraRoll, Image, FlatList, ActivityIndicator, Dimensions} from 'react-native';
+import Touchable from '@appandflow/touchable';
 
 const MAX_PHOTOS = 20;
+const PADDING = 17;
+const MARGIN = 10;
+const {width} = Dimensions.get('window');
 const styles =StyleSheet.create({
     imageWrapper: {
-        width: 100,
-        height: 100,
+        width: (width - PADDING * 2 - MARGIN * 2) / 3,
+        height: (width - PADDING * 2 - MARGIN * 2) / 3,
+        borderRadius: 3,
+        marginHorizontal: MARGIN,
+        marginVertical: '2.5%',
     },
     image: {
         flex: 1,
+        borderRadius: 3,
     },
-
+    loadingWrapper: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    imageHover: {
+        position: 'absolute',
+        top: 0,
+        right: 0,
+        left: 0, 
+        bottom: 0,
+        borderRadius: 3,
+        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    },
 });
 class CreatePhotoScreen extends PureComponent {
     state = {
-        images: []
+        images: [],
+        loading: false,
+        selected: null,
+        hasNextPage: false,
+        endCursor: '',
+        firstQuery: true,
     };
     componentDidMount() {
         this._getPhotos();
     }
     _getPhotos = async after => {
+        if (this.state.firstQuery) {
+            this.setState({loading: true}); 
+        }
         const res = await CameraRoll.getPhotos({
             first: MAX_PHOTOS,
             after,
         });
         this.setState({
-            images: [...this.state.images, res.edges],
+            images: [...this.state.images, ...res.edges],
+            loading: false,
+            firstQuery: false,
+            hasNextPage: res.page_info.has_next_page,
+            endCursor: res.has_next_page.endCursor,
         });
     };
     _renderItem = ({item}) => {
+        const isSelected = this.state.selected && this.state.selected.node.image.filename === item.node.image.filename;
         return (
-            <View style={styles.imageWrapper}>
+            <Touchable 
+                feedback="opacity"
+                onPress={() => this._onSelect(item)}
+                disabled={isSelected}
+                style={styles.imageWrapper}
+            >
                 <Image source={{uri: item.node.image.uri}} style={styles.image}/>
-            </View>
-        )
-    }
-    _keyExtractor = (item) => item.node.image.filename
+                {isSelected && <View style={styles.imageHover}/>}
+            </Touchable>
+        );
+    };
+    _keyExtractor = (item) => item.node.image.filename;
+    _onSelect = (selected) => {
+        this.setState({selected});
+    };
+    _onEndReached = () => {
+        if(this.state.hasNextPage) {
+            this._getPhotos(this.state.endCursor);    
+        }
+    };
     render() {
+        if(this.state.loading) {
+            return (
+                <View style={styles.loadingWrapper}>
+                    <ActivityIndicator />
+                </View>
+            );
+        }
         return (
             <FlatList 
                 data={this.state.images}
                 renderItem={this._renderItem}
                 keyExtractor={this._keyExtractor}
+                numColumns={3}
+                extraData={this.state}
+                onEndReached={this._onEndReached}
             />
         );
     }
