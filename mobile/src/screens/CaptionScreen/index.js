500dd1157e4366386e8d6579c8904d7ff8c4537f
Zehan Sunesara <usazehan@gmail.com>
2018-07-30 16:21:58 -0500

    uploading image to AWS S3

--- mobile/src/screens/CaptionScreen/index.js
+++ mobile/src/screens/CaptionScreen/index.js
@@ -4,7 +4,18 @@ import {Divider} from '../../components';
 import Touchable from '@appandflow/touchable';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import {colors} from '../../utils/themes';
+import gql from 'graphql-tag';
+import {withApollo} from 'react-apollo';
+import {uploadImageToS3} from '../../utils/uploadImage';
 
+const signS3Query = gql`
+    query {
+        presignUrl {
+            url
+            uploadUrl
+        }
+    }
+`;
 const styles = StyleSheet.create({
     root: {
         flex: 1,
@@ -46,8 +57,40 @@ const styles = StyleSheet.create({
 });
 
 class CaptionScreen extends PureComponent {
-    state = {
-        caption: ''
+    constructor(props) {
+        super(props); 
+        this.state = {
+            caption: ''
+        };
+        props.navigator.setOnNavigatorEvent(this._onNavigatorEvent.bind(this));
+    }
+    componentDidMount() {
+        this.props.navigator.setButtons({
+            rightButtons: [
+                {
+                    id: 'sharePost',
+                    title: 'Share',
+                },
+            ],
+            animated: true,
+        });
+    }
+    _onNavigatorEvent = e => {
+        if (e.type === 'NavBarButtonPress') {
+            if (e.id === 'sharePost') {
+                this._onSharePostPress();
+            }
+        }
+    }
+    _onSharePostPress = async () => {
+        const res = await this.props.client.query({query: signS3Query});
+        const resultFromS3 = await uploadImageToS3(
+            this.props.image.node.image.uri,
+            res.data.presignUrl
+        );
+        console.log('=======================================');
+        console.log('resultFromS3', resultFromS3);
+        console.log('=======================================');
     };
     _onCaptionChange = caption => this.setState({caption});
     render() {
@@ -89,4 +132,4 @@ class CaptionScreen extends PureComponent {
     }
 }
 
-export default CaptionScreen;
+export default withApollo(CaptionScreen);
