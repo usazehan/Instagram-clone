fbbc60456c583a8d8d0a9a08d798a7375d800450
Zehan Sunesara <usazehan@gmail.com>
2018-08-14 12:38:41 -0500

    Search by tag name

--- instagram/lib/instagram/posts/posts.ex
+++ instagram/lib/instagram/posts/posts.ex
@@ -6,10 +6,10 @@ defmodule Instagram.Posts do
   import Ecto.Query, warn: false
   alias Instagram.Repo
 
-  alias Instagram.Posts.Photo
+  alias Instagram.Posts.{Photo, Comment, Tag}
 
   def list_photos do
-  query = from p in Photo, order_by: [desc: :inserted_at]
+    query = from p in Photo, order_by: [desc: :inserted_at]
     Repo.all(query)
   end
 
@@ -83,99 +83,9 @@ defmodule Instagram.Posts do
     Repo.all(query)
   end
 
-  alias Instagram.Posts.Comment
+  def search_tag(name) do
+    term = "%#{String.trim(name)}%"
 
-  @doc """
-  Returns the list of photo_comments.
-
-  ## Examples
-
-      iex> list_photo_comments()
-      [%Comment{}, ...]
-
-  """
-  def list_photo_comments do
-    Repo.all(Comment)
-  end
-
-  @doc """
-  Gets a single comment.
-
-  Raises `Ecto.NoResultsError` if the Comment does not exist.
-
-  ## Examples
-
-      iex> get_comment!(123)
-      %Comment{}
-
-      iex> get_comment!(456)
-      ** (Ecto.NoResultsError)
-
-  """
-  def get_comment!(id), do: Repo.get!(Comment, id)
-
-  @doc """
-  Creates a comment.
-
-  ## Examples
-
-      iex> create_comment(%{field: value})
-      {:ok, %Comment{}}
-
-      iex> create_comment(%{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
-  """
-  def create_comment(attrs \\ %{}) do
-    %Comment{}
-    |> Comment.changeset(attrs)
-    |> Repo.insert()
-  end
-
-  @doc """
-  Updates a comment.
-
-  ## Examples
-
-      iex> update_comment(comment, %{field: new_value})
-      {:ok, %Comment{}}
-
-      iex> update_comment(comment, %{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
-  """
-  def update_comment(%Comment{} = comment, attrs) do
-    comment
-    |> Comment.changeset(attrs)
-    |> Repo.update()
-  end
-
-  @doc """
-  Deletes a Comment.
-
-  ## Examples
-
-      iex> delete_comment(comment)
-      {:ok, %Comment{}}
-
-      iex> delete_comment(comment)
-      {:error, %Ecto.Changeset{}}
-
-  """
-  def delete_comment(%Comment{} = comment) do
-    Repo.delete(comment)
-  end
-
-  @doc """
-  Returns an `%Ecto.Changeset{}` for tracking comment changes.
-
-  ## Examples
-
-      iex> change_comment(comment)
-      %Ecto.Changeset{source: %Comment{}}
-
-  """
-  def change_comment(%Comment{} = comment) do
-    Comment.changeset(comment, %{})
+    Repo.all from t in Tag, where: ilike(t.name, ^term)
   end
 end
